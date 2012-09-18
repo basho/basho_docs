2b97ea6db8bb5129e3c8a03bd64f534f4bb52c19
John Newman <rescuecreative@gmail.com>
2012-09-18 17:14:38 -0400

    added some fonts and background images and scss, rearranged the css a bit

--- source/js/all.js
+++ source/js/all.js
@@ -1,1 +1,34 @@
 //= require_tree .
+
+// Don't throw an error if jQuery doesn't load
+(typeof jQuery !== 'function') ? null : jQuery(function () {
+
+  var $ = jQuery,
+      contentMargin;
+      
+  function getContentMargin() {
+    contentMargin = $('div[role=main]').css('margin-left');
+  }
+  getContentMargin();
+  
+  function closeNav() {
+    $('#primary-nav').fadeOut();
+    $('div[role=main]').animate({marginLeft: '24px'}, {queue: false, duration: 300});
+  }
+  
+  function openNav() {
+    var cm = contentMargin;
+    $('#primary-nav').fadeIn();
+    $('div[role=main]').animate({marginLeft: cm}, {queue: false, duration: 300});
+  }
+  
+  $(window).on('resize', getContentMargin);
+  $(document).on('click', '#nav-toggle', function () {
+    if ($('#primary-nav').is(':hidden')) {
+      openNav();
+    } else {
+      closeNav();
+    }
+  });
+
+});
