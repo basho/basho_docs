a6bab640a78f2df97e1a4953dfb43ee3b7052c5e
Eric Redmond <eredmond@basho.com>
2012-09-19 16:34:59 -0700

    remove dsstores

--- lib/index.rb
+++ lib/index.rb
@@ -1,4 +1,3 @@
-
 ROOT_URL = "http://localhost:8091/riak/docs"
 
 def make_riak_key(resource)
@@ -15,28 +14,31 @@ def build_yokozuna_index(resources)
     metadata['url'] = resource.url.sub(/\.html/, '/')
 
     key = make_riak_key(resource)
-    data = File.read(resource.source_file)
-    # remove the top metadata
-    data.sub!(/^\-{3}$.*?^\-{3}$/m, '')
+    body = File.read(resource.source_file)
+    data = body
 
-    xml = Builder::XmlMarkup.new(:target=>'')
-    # xml.instruct!(:xml, :encoding => "UTF-8")
-    xml.add do |add|
-      add.doc do |doc|
-        doc.field({:name=>'title'}, metadata['title'])
-        doc.field({:name=>'project'}, metadata['project'])
-        doc.field({:name=>'doctype'}, metadata['document'])
-        doc.field({:name=>'index'}, metadata['index'] == 'true')
-        doc.field({:name=>'audience'}, metadata['audience'])
-        doc.field({:name=>'keywords'}, (metadata['keywords']||[]).join(','))
-        doc.field({:name=>'body'}, data)
-      end
-    end
+    # # remove the top metadata
+    # data.sub!(/^\-{3}$.*?^\-{3}$/m, '')
+
+    # xml = Builder::XmlMarkup.new(:target=>'')
+    # # xml.instruct!(:xml, :encoding => "UTF-8")
+    # xml.add do |add|
+    #   add.doc do |doc|
+    #     doc.field({:name=>'title'}, metadata['title'])
+    #     doc.field({:name=>'project'}, metadata['project'])
+    #     doc.field({:name=>'doctype'}, metadata['document'])
+    #     doc.field({:name=>'index'}, metadata['index'] == 'true')
+    #     doc.field({:name=>'audience'}, metadata['audience'])
+    #     doc.field({:name=>'keywords'}, (metadata['keywords']||[]).join(','))
+    #     doc.field({:name=>'body'}, body)
+    #   end
+    # end
+    # data = xml.target!
 
     command = <<-CURL
     curl '#{ROOT_URL}/#{key}' \
     -H 'content-type:text/plain' -X PUT --data-binary @-<<\\YNFCM
-    #{xml.target!}
+    #{data}
     YNFCM
     CURL
 
