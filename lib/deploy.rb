0183df18f349d12e974868575be31c1b3f61645b
Eric Redmond <eredmond@basho.com>
2012-09-28 15:27:10 -0400

    add font to deploy

--- lib/deploy.rb
+++ lib/deploy.rb
@@ -46,7 +46,7 @@ module ::Middleman::Features::Deploy
           # a copy of the full site to reflect the latest values
 
           # anything under images, js, css goes to "shared"
-          if f =~ /^\.\/build\/(?:images|js|css)\//
+          if f =~ /^\.\/build\/(?:images|js|css|fonts)\//
             # upload shared for all given project versions
             $versions.values.uniq.each do |version|
               key = f.sub(/\.\/build\//, "shared/#{version}/")
