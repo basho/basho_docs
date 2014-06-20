709feee9849dfbf1267b090188a3b4e4fc66b949
Luc Perkins <lucperkins@gmail.com>
2014-06-20 15:56:34 +0200

    small changes

--- pdf/generate_index.rb
+++ pdf/generate_index.rb
@@ -3,7 +3,7 @@ require 'yaml'
 def generate_index_markdown
   markdown_string = String.new
   markdown_string.concat("Welcome to the Riak Docs!\n")
-  riak_nav = YAML.load_file('source/languages/en/global_nav.yml')['riak']
+  riak_nav = YAML.load_file('../source/languages/en/global_nav.yml')['riak']
   Array(0..riak_nav.length - 1).each do |section|
     markdown_string.concat("\n## #{riak_nav[section]['title']}\n\n")
     subsections = riak_nav[section]['sub']
@@ -16,15 +16,13 @@ def generate_index_markdown
             markdown_string.concat("#{ss}\n")
           elsif ss.is_a? Hash
             ss['sub'].each do |sss|
-              markdown_string.concat("#{sss}\n")
+              markdown_string.concat("#{sss}\n\n")
             end
           end
         end
       end
     end
   end
-  p markdown_string
-  File.write('pdf/build/index.md', markdown_string)
+  File.write('./build/index.md', markdown_string)
+  p 'Generated a Markdown index in /build out of global_nav.yml'
 end
-
-generate_index_markdown
--- pdf/pdfgen.rb
+++ pdf/pdfgen.rb
@@ -8,43 +8,49 @@ class String
   end
 end
 
+# Directories
+$here = File.expand_path(File.dirname(__FILE__))
+
+# Gather configuration (none yet)
+# $config = YAML.load_file('#$here/config.yml')
+
+# List of pages not to render (TODO)
 $dont_render = ['release-notes.md']
 
-markdown_toc_parser = Redcarpet::Markdown.new(Redcarpet::Render::HTML_TOC.new(),
+$markdown_parser = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(),
   :fenced_code_blocks => true,
   :autolink => true,
   :no_intra_emphasis => true,
   :space_after_headers => false
 )
 
-markdown_parser = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(),
-  :fenced_code_blocks => true,
-  :autolink => true,
-  :no_intra_emphasis => true,
-  :space_after_headers => false
-)
+def generate_pdf
+  # Which sections to render (TODO)
+  dirs_to_render = ['dev', 'ops', 'theory']
 
-$html_string = ""
+  $html_string = String.new
+  $html_string << $markdown_parser.render(File.read('build/index.md'))
 
-dirs_to_render = ['dev', 'ops', 'theory']
-
-$html_string << markdown_parser.render(File.read('build/index.md'))
-
-Dir['../source/languages/en/riak/**/*.md'].each do |file|
-  if !($dont_render.include? file)
-    raw_file_contents = File.read(file)
-    raw_file_contents.gsub!(/\/images/, '../source/images')
-    $html_string << markdown_parser.render(raw_file_contents)
+  Dir['../source/languages/en/riak/**/*.md'].each do |file|
+    if !($dont_render.include? file)
+      raw_file_contents = File.read(file)
+      raw_file_contents.del!(/---(.*)---/m)
+      raw_file_contents.gsub!(/\/images/, '../source/images')
+      $html_string << $markdown_parser.render(raw_file_contents)
+    end
   end
+
+  final_string = "<html><head><title>The Riak Docs</title></head><body>#{$html_string}</body></html>"
+
+  puts 'Generating an HTML file at /build/docs.html...'
+  File.write('build/docs.html', final_string)
+
+  puts 'Generating PDF...'
+
+  # Bust out Pandoc
+  `pandoc build/docs.html -o docs.pdf --latex-engine=xelatex`
+
+  puts 'Generation complete!'
 end
 
-final_string = """<html>
-  <head>
-    <title>The Riak Documentation</title>
-  </head>
-  <body>
-    #{$html_string}
-  </body>
-</html>"
-
-File.write('build/docs.html', final_string)
+generate_pdf
