1edff70e4bf591b29fc2ec05f7d148f7fe5d22a7
Drew Pirrone-Brusse <drew.pirrone.brusse@gmail.com>
2016-08-15 14:30:06 -0400

    Add JS vendor libraries to head.js and main.js

--- dynamic/js/head.js
+++ dynamic/js/head.js
@@ -1,1 +1,15 @@
-// head.js
+/* <head> JS
+ * =========
+ * The full set of JS that needs to be included in the <head> element and run
+ * before the <body> element begins to load.
+ *
+ * This should **only** include JS required to avoid a FOUC.
+ */
+
+/* Vendor Library Includes
+ * -----------------------
+ * Included one at a time to ensure all requirements are met
+ */
+//= require ./vendor/modernizr-3.1.1.js
+
+// TODO: Find out if highlight.js should be added in here.
